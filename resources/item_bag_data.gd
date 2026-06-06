18d6d57745cfb8d36505889c23c672e9c77f0bcc
Arlo Purcell <apurcell@clone.me>
2026-06-06 12:39:53 -0400

    feat: add ItemData and ItemBagData resource classes

---
+++ resources/item_bag_data.gd
@@ -0,0 +1,4 @@
+class_name ItemBagData
+extends Resource
+
+@export var items: Array[ItemData] = []
---
+++ resources/item_data.gd
@@ -0,0 +1,7 @@
+class_name ItemData
+extends Resource
+
+@export var item_name: String = ""
+@export var color: String = ""      # "red", "blue", or "yellow"
+@export var strength: int = 1       # 1–6
+@export var location: int = 0       # space_id where this item is placed on the board
