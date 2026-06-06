9ca0e841807d294cd8dc536b25052d81ba0cbaf5
Arlo Purcell <apurcell@clone.me>
2026-06-06 11:49:08 -0400

    feat: draw connection lines between neighboring spaces

--- scenes/Board.gd
+++ scenes/Board.gd
@@ -3,13 +3,30 @@ extends Node2D
 
 const SpaceNodeScene := preload("res://scenes/SpaceNode.tscn")
 
+const CONNECTION_COLOR := Color(0.353, 0.235, 0.1, 0.5)
+const CONNECTION_WIDTH := 4.0
+
 var _space_nodes: Dictionary = {}   # space_id (int) -> SpaceNode
+var _board_data: BoardData = null
 
 func _ready() -> void:
-    var board_data: BoardData = load("res://resources/data/board.tres")
-    _spawn_spaces(board_data)
+    _board_data = load("res://resources/data/board.tres")
+    _spawn_spaces(_board_data)
     GameManager.turn_changed.connect(_on_turn_changed)
     GameManager.player_moved.connect(_on_player_moved)
+
+func _draw() -> void:
+    if _board_data == null:
+        return
+    for space_data in _board_data.spaces:
+        if not space_data.id in _space_nodes:
+            continue
+        var from := _space_nodes[space_data.id].position + SpaceNode.SIZE / 2
+        for neighbor_id in space_data.neighbors:
+            if neighbor_id <= space_data.id or not neighbor_id in _space_nodes:
+                continue
+            var to := _space_nodes[neighbor_id].position + SpaceNode.SIZE / 2
+            draw_line(from, to, CONNECTION_COLOR, CONNECTION_WIDTH, true)
 
 func get_space_center(space_id: int) -> Vector2:
     if space_id in _space_nodes:
