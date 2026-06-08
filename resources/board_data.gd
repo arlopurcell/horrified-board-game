44a4fac37f3b8ac71fa84da98abe4c6688027af4
Arlo Purcell <apurcell@clone.me>
2026-06-08 12:46:09 -0400

    Adding basic monster system

--- resources/board_data.gd
+++ resources/board_data.gd
@@ -4,7 +4,7 @@ extends Resource
 @export var spaces: Array[SpaceData] = []
 
 func get_space(space_id: int) -> SpaceData:
-    for space in spaces:
-        if space.id == space_id:
-            return space
-    return null
+	for space in spaces:
+		if space.id == space_id:
+			return space
+	return null
